4e7d4385beee94cbd8cbc0e91b30a9cbdf0a3a26
Pepe Corredera <jcorredera@datiobd.com>
2017-02-18 15:11:33 +0100

    [~] refactorizo nombres

--- web-cluster/output.tf
+++ web-cluster/output.tf
@@ -2,9 +2,9 @@
 # Instance IP
 # -----------------------------------------------------
 output "web-cluser-floating-ip" {
-  value = "${openstack_compute_floatingip_v2.floatip_1.address}"
+  value = "${openstack_compute_floatingip_v2.web-server-floatip.address}"
 }
 
 output "web-cluster-ip-list" {
-  value = ["${openstack_compute_instance_v2.web_server.*.access_ip_v4}"]
+  value = ["${openstack_compute_instance_v2.web-server.*.access_ip_v4}"]
 }
