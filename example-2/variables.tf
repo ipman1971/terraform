ff45a9902318bc309ff8728954a2e2f43a950892
Pepe Corredera <jcorredera@datiobd.com>
2017-02-22 16:40:46 +0100

    [+] inicio de example-2

---
+++ example-2/variables.tf
@@ -0,0 +1,46 @@
+# -----------------------------------------------------
+# Openstack credentials
+# -----------------------------------------------------
+variable "image_selected" {
+  default = "1d3593f7-26ef-464e-9936-9fedce135e04"
+}
+
+variable "user_name" {
+  default = "devops"
+}
+
+variable "password" {}
+
+variable "project" {
+  default = "iac-dev"
+}
+
+variable "auth_url" {
+  default = "http://javamaster.ovh:5000/v2.0"
+}
+
+# -----------------------------------------------------
+# Access to instances
+# -----------------------------------------------------
+variable "user_instance" {
+  default = "debian"
+}
+
+variable "ssh_key_file" {
+  default = "../key-pairs/instance.key"
+}
+
+variable "flavor_name" {
+  default = "ds512M"
+}
+
+# -----------------------------------------------------
+# Connection to world
+# -----------------------------------------------------
+variable "pool" {
+  default = "public"
+}
+
+variable "public_net" {
+  default = "4626acf5-0c3a-4900-916c-946e06a4ac06"
+}
